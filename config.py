a0d270f26c62ebcd8a060f93cb9bc7baf466a54d
Chengxin Zhao <99518086+ChesterZhao@users.noreply.github.com>
2024-04-09 17:45:18 +0800

    updated production config

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class TestConfig:
     TESTING = True
     DEBUG = True
@@ -13,4 +15,4 @@ class DevConfig:
 class ProdConfig:
     Testing = False
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:20020316@127.0.0.1:3306/pickr"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://flask:{os.environ.get('FLASK_DV_PASSWORD')}@localhost/studentprojectmanager"
